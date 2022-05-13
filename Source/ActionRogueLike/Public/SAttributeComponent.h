8ae198ddf4939ce1eec242bcd68f450812b634eb
oscar420 <66745768+oscar420@users.noreply.github.com>
2022-05-13 17:55:43 -0300

    Attribute Component
    -Added Attribute component class.
    -Added Health variable and HealthChange function

---
+++ Source/ActionRogueLike/Public/SAttributeComponent.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SAttributeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ACTIONROGUELIKE_API USAttributeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USAttributeComponent();
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Attribute")
+	float Health;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category="Attribute")
+	bool ApplyHealthChange(float Delta);
+	
+};
